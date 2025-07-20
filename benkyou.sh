a9d6d5c07bc57abf1a268f47e548675129aceec3
suzutsuki0114 <209620833+suzutsuki0114@users.noreply.github.com>
2025-07-20 21:10:04 +0900

    Revert "コード可読性向上"
    This reverts commit fbb6d1c57a659fd840212f3619e11118f75b1c71.

--- benkyou.sh
+++ benkyou.sh
@@ -4,10 +4,6 @@ CHARS=('見' 'て' 'る' '暇' 'が' 'あ' 'っ' 'た' 'ら' '勉' '強' 'し' '
 
 while true; do
     for ((i=0; i<${#CHARS[@]}; i++)); do
-        printf "\033[1m\
-            \033[48;2;$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1)m\
-            \033[38;2;$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1)m\
-            ${CHARS[$i]}\
-            \033[0m"
+        printf "\033[1m\033[48;2;$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1)m\033[38;2;$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1);$(shuf -i 0-255 -n 1)m${CHARS[$i]}\033[0m"
     done
 done
